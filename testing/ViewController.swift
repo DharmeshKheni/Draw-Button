ee0f93bc013e84f5d6a78fe9c172b5f2b07a0b1a
Dharmesh <khenid21@gmail.com>
2015-06-24 17:45:55 +0530

    Draw Button
    Draw Button

---
+++ testing/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  testing
+//
+//  Created by Anil on 24/06/15.
+//  Copyright (c) 2015 Variya Soft Solutions. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    // test value
+    let test : String = "test"
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    
+        
+    }
+}
+
